3cb9c549fb568cd655307868e719fff8da3a8b4c
renan-klinsmann <klismann-nan@hotmail.com>
2021-11-29 17:38:33 -0300

    Correcao de token

--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { OrderPayload } from "./types";
 
-const API_URL = process.env.REACT_APP_API_URL;
-const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+const API_URL = 'https://renan-dsdeliver-sds.herokuapp.com'//process.env.REACT_APP_API_URL;
+const mapboxToken = 'pk.eyJ1IjoicmVuYW5tZW5kb25jYTE5OTQiLCJhIjoiY2t3a3o3bXRsMXhjMTJ1b3ppMnVncGFobCJ9.qNMoYtP7RBxGs9M6v4R1fw'; //process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 export function fetchProducts (){
     return axios(`${API_URL}/products`)
 }
